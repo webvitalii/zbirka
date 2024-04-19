fa5157cae2d29e7d23fc063d370d159080409a5f
Vitalii <vitalii.skorokhod@gmail.com>
2024-04-19 17:01:48 -0400

    Update menu/layout logic

--- zbirka.py
+++ zbirka.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QMenu)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QMenu, QStackedLayout)
 from PySide6.QtGui import QIcon, QAction
 
 
@@ -9,23 +9,20 @@ class App(QMainWindow):
         self.setWindowTitle("Python GUI")
         self.setFixedSize(800, 600) # width, height
         self.setWindowIcon(QIcon('assets/icon-ua-flag.ico'))
-        
-        layout = QVBoxLayout()
-        
-        self.inputField = QLineEdit()
-        self.textField = QTextEdit() # multiline text input
-        self.button = QPushButton("Greet")
-        self.button.clicked.connect(self.sayHi)
-        layout.addWidget(self.inputField)
-        layout.addWidget(self.textField)
-        layout.addWidget(self.button)
-        
-        centralWidget = QWidget()
-        centralWidget.setLayout(layout)
-        self.setCentralWidget(centralWidget)
-        
+
+        self.content_layout = QStackedLayout()
+        self.setCentralWidget(QWidget(self))
+        self.centralWidget().setLayout(self.content_layout)
+
         self.create_menu()
         
+        self.greet_page = GreetPage()
+        self.content_layout.addWidget(self.greet_page)
+        
+        self.whois_page = WhoisPage()
+        self.content_layout.addWidget(self.whois_page)
+
+
     def create_menu(self):
         menu = self.menuBar()
         fileMenu = menu.addMenu('File')
@@ -35,43 +32,62 @@ class App(QMainWindow):
         impAct = QAction('Import mail', self)
         impMenu.addAction(impAct)
 
-        greetAct = QAction('Greet', self)
-        greetAct.triggered.connect(self.greetPage)
+        greet_action = QAction('Greet', self)
+        greet_action.triggered.connect(self.show_greet)
         
-        whoisAct = QAction('WhoIs', self)
-        whoisAct.triggered.connect(self.whoIsPage)
+        whois_action = QAction("Whois", self)
+        whois_action.triggered.connect(self.show_whois)
 
-
-        toolsMenu.addAction(greetAct)
-        toolsMenu.addAction(whoisAct)
+        toolsMenu.addAction(greet_action)
+        toolsMenu.addAction(whois_action)
         fileMenu.addMenu(impMenu)
+
+    def show_whois(self):
+        self.content_layout.setCurrentWidget(self.whois_page)
+        
+    def show_greet(self):
+        self.content_layout.setCurrentWidget(self.greet_page)
 
     def sayHi(self):
         inputText = self.inputField.text()
         self.textField.setText(f"Hi, {inputText}!")
 
 
-    def whoIsPage(self):
-        inputField = QLineEdit()
-        textField = QTextEdit() # multiline text input
-        button = QPushButton("Whois")
-        layout = QVBoxLayout()
-        layout.addWidget(textField)
-        layout.addWidget(button)
-        centralWidget = QWidget()
-        centralWidget.setLayout(layout)
-        self.setCentralWidget(centralWidget)
+class WhoisPage(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+
+        self.text_input = QLineEdit()
+        self.layout.addWidget(self.text_input)
+
+        self.submit_btn = QPushButton("Whois class")
+        self.layout.addWidget(self.submit_btn)
+
+        self.setLayout(self.layout)
         
-    def greetPage(self):
-        inputField = QLineEdit()
-        textField = QTextEdit() # multiline text input
-        button = QPushButton("Greet")
-        layout = QVBoxLayout()
-        layout.addWidget(inputField)
-        layout.addWidget(button)
-        centralWidget = QWidget()
-        centralWidget.setLayout(layout)
-        self.setCentralWidget(centralWidget)
+        
+class GreetPage(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+
+        self.text_input = QLineEdit()
+        self.layout.addWidget(self.text_input)
+
+        self.submit_btn = QPushButton("Greet class")
+        self.layout.addWidget(self.submit_btn)
+
+        self.setLayout(self.layout)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
